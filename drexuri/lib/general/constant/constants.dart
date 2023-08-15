a8c83c4eddb883978edab16c5a205f66b8b812fa
Emmanuel Balogun <80977358+DreXuri@users.noreply.github.com>
2023-08-15 13:58:26 +0100

    setting up project

---
+++ drexuri/lib/general/constant/constants.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+
+final Color primaryHexColor = HexColor('#0B9C07');
+final Color primaryIcon = HexColor('#AEDEAC');
+final Color splashColor = HexColor('#22A1E6');
+final Color secondaryHexColor = HexColor('#FFA531');
+final Color neutralColor = HexColor('#00353D');
+final Color blackHex = HexColor('#2B2B2B');
+final Color whiteHex = HexColor('#ffffff');
+final Color iconHex = HexColor('#00353D');
+final Color redHex = HexColor('FF0000');
+final Color greyHex = HexColor('##CCCCCC');
+const backgroundColor = Colors.white;
+const greyBackgroundCOlor = Color(0xffebecee);
+// final selectedNavBarColor = Colors.cyan[800]!;
+final Color selectedNavBarColor = HexColor('#808080');
+
+// final unselectedNavBarColor = Colors.black87;
+final Color unselectedNavBarColor = HexColor('#2B2B2B');
---
+++ drexuri/lib/general/utils/constants.dart
@@ -0,0 +1,14 @@
+// import 'package:flutter/material.dart';
+// import 'package:google_fonts/google_fonts.dart';
+
+// final kStyleInter = GoogleFonts.inter(
+//   textStyle: TextStyle(color: blackHex),
+// );
+
+// final kStylePoppins = GoogleFonts.poppins(
+//   textStyle: TextStyle(color: blackHex),
+// );
+
+// final kStyleAgustina = GoogleFonts.agustina(
+//   textStyle: TextStyle(color: blackHex),
+// );
